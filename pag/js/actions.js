48c9c915466cf92bc6b0c37063a540e62ff09b90
managua2019 <marvin16will@hotmail.com>
2019-12-05 12:44:13 -0600

    Desarollo

---
+++ pag/js/actions.js
@@ -0,0 +1,18 @@
+$("#Retorno").click(function() {
+	// body...
+	window.location="../index.html"
+});
+
+$("#Page1").click(function() {
+	//body...
+	window.location="./galeria.html"
+});
+
+$("#Page2").click(function() {
+	//body...
+	window.location="./pdf.html"
+});
+
+$("#Salir").click(function(){
+	window.location="./kk.html"
+});
